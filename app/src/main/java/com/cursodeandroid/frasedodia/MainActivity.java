fd59d7c3670c525a4c7f06be45bfd730bb352281
pg <pinguimg@gmail.com>
2021-02-17 21:10:11 -0300

    First Commit

---
+++ app/src/main/java/com/cursodeandroid/frasedodia/MainActivity.java
@@ -0,0 +1,39 @@
+package com.cursodeandroid.frasedodia;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private TextView textoNovaFrase;
+    private Button botaoNovaFrase;
+
+    private String[] frases = {"teste" , "teste2", "teste 3"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textoNovaFrase = findViewById(R.id.textoNovaFraseid);
+        botaoNovaFrase = findViewById(R.id.botaoNovaFraseid);
+
+
+
+        botaoNovaFrase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Random randomico = new Random();
+                int numeroAleatorio = randomico.nextInt(frases.length);
+                textoNovaFrase.setText(frases[numeroAleatorio]);
+            }
+        });
+    }
+}
